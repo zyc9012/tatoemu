7a415c2b0d2cc222a8fff31cfe4d1676f1d03cb1
zyc9012 <zyc9012@gmail.com>
2026-01-13 08:39:23 +0800

    [CPS1] Disable sound interpolation

--- emulators/components/sound/burnint.cpp
+++ emulators/components/sound/burnint.cpp
@@ -5,6 +5,6 @@
 #include "burnint.h"
 
 INT32 nBurnSoundRate = 44100;
-INT32 nInterpolation = 3;  // Linear interpolation
+INT32 nInterpolation = 0;
 UINT8 DebugSnd_MSM6295Initted = 0;
 UINT8 DebugSnd_AY8910Initted = 0;
