cc2c57f1a4b5347842cd94ab665dc2f5e9903614
zyc9012 <zyc9012@gmail.com>
2026-02-08 18:42:38 +0800

    [Web] Support key binding settings

--- platforms/wasm/main.cpp
+++ platforms/wasm/main.cpp
@@ -1,5 +1,8 @@
 #include "emulator.h"
 #include "config.h"
+#include "nes/config.h"
+#include "gb/config.h"
+#include "cps/config.h"
 #include "neogeo/config.h"
 #include <SDL3/SDL.h>
 #include <emscripten.h>
@@ -112,6 +115,96 @@ extern "C" {
             biosIndex = neogeo::Config::BiosIndex;
         }
         neogeo::Config::BiosIndex = biosIndex;
+    }
+
+    void setKeyBinding(const char* section, const char* key, const char* value) {
+        SDL_Keycode kc = SDL_GetKeyFromName(value);
+        if (kc == SDLK_UNKNOWN) {
+            log_error("Unknown key name: %s", value);
+            return;
+        }
+
+        std::string sec(section);
+        std::string k(key);
+
+        if (sec == "Common") {
+            if (k == "Quit") Config::Key::Quit = kc;
+            else if (k == "SaveState") Config::Key::SaveState = kc;
+            else if (k == "LoadState") Config::Key::LoadState = kc;
+            else if (k == "Pause") Config::Key::Pause = kc;
+            else if (k == "SpeedUp") Config::Key::GameSpeedUp = kc;
+            else if (k == "SpeedDown") Config::Key::GameSpeedDown = kc;
+        } else if (sec == "GB") {
+            if (k == "A") gb::Config::Key::ButtonA = kc;
+            else if (k == "B") gb::Config::Key::ButtonB = kc;
+            else if (k == "Start") gb::Config::Key::Start = kc;
+            else if (k == "Select") gb::Config::Key::Select = kc;
+            else if (k == "Up") gb::Config::Key::DpadUp = kc;
+            else if (k == "Down") gb::Config::Key::DpadDown = kc;
+            else if (k == "Left") gb::Config::Key::DpadLeft = kc;
+            else if (k == "Right") gb::Config::Key::DpadRight = kc;
+        } else if (sec == "NES") {
+            if (k == "A") nes::Config::Key::ButtonA = kc;
+            else if (k == "B") nes::Config::Key::ButtonB = kc;
+            else if (k == "Start") nes::Config::Key::Start = kc;
+            else if (k == "Select") nes::Config::Key::Select = kc;
+            else if (k == "Up") nes::Config::Key::DpadUp = kc;
+            else if (k == "Down") nes::Config::Key::DpadDown = kc;
+            else if (k == "Left") nes::Config::Key::DpadLeft = kc;
+            else if (k == "Right") nes::Config::Key::DpadRight = kc;
+        } else if (sec == "CPS") {
+            if (k == "P1_Up") cps::Config::Key::P1_Up = kc;
+            else if (k == "P1_Down") cps::Config::Key::P1_Down = kc;
+            else if (k == "P1_Left") cps::Config::Key::P1_Left = kc;
+            else if (k == "P1_Right") cps::Config::Key::P1_Right = kc;
+            else if (k == "P1_Punch1") cps::Config::Key::P1_Punch1 = kc;
+            else if (k == "P1_Punch2") cps::Config::Key::P1_Punch2 = kc;
+            else if (k == "P1_Punch3") cps::Config::Key::P1_Punch3 = kc;
+            else if (k == "P1_Kick1") cps::Config::Key::P1_Kick1 = kc;
+            else if (k == "P1_Kick2") cps::Config::Key::P1_Kick2 = kc;
+            else if (k == "P1_Kick3") cps::Config::Key::P1_Kick3 = kc;
+            else if (k == "P2_Up") cps::Config::Key::P2_Up = kc;
+            else if (k == "P2_Down") cps::Config::Key::P2_Down = kc;
+            else if (k == "P2_Left") cps::Config::Key::P2_Left = kc;
+            else if (k == "P2_Right") cps::Config::Key::P2_Right = kc;
+            else if (k == "P2_Punch1") cps::Config::Key::P2_Punch1 = kc;
+            else if (k == "P2_Punch2") cps::Config::Key::P2_Punch2 = kc;
+            else if (k == "P2_Punch3") cps::Config::Key::P2_Punch3 = kc;
+            else if (k == "P2_Kick1") cps::Config::Key::P2_Kick1 = kc;
+            else if (k == "P2_Kick2") cps::Config::Key::P2_Kick2 = kc;
+            else if (k == "P2_Kick3") cps::Config::Key::P2_Kick3 = kc;
+            else if (k == "P1_Coin") cps::Config::Key::P1_Coin = kc;
+            else if (k == "P2_Coin") cps::Config::Key::P2_Coin = kc;
+            else if (k == "P1_Start") cps::Config::Key::P1_Start = kc;
+            else if (k == "P2_Start") cps::Config::Key::P2_Start = kc;
+            else if (k == "Diag") cps::Config::Key::Diag = kc;
+            else if (k == "Service") cps::Config::Key::Service = kc;
+        } else if (sec == "NeoGeo") {
+            if (k == "P1_Up") neogeo::Config::Key::P1_Up = kc;
+            else if (k == "P1_Down") neogeo::Config::Key::P1_Down = kc;
+            else if (k == "P1_Left") neogeo::Config::Key::P1_Left = kc;
+            else if (k == "P1_Right") neogeo::Config::Key::P1_Right = kc;
+            else if (k == "P1_A") neogeo::Config::Key::P1_ButtonA = kc;
+            else if (k == "P1_B") neogeo::Config::Key::P1_ButtonB = kc;
+            else if (k == "P1_C") neogeo::Config::Key::P1_ButtonC = kc;
+            else if (k == "P1_D") neogeo::Config::Key::P1_ButtonD = kc;
+            else if (k == "P2_Up") neogeo::Config::Key::P2_Up = kc;
+            else if (k == "P2_Down") neogeo::Config::Key::P2_Down = kc;
+            else if (k == "P2_Left") neogeo::Config::Key::P2_Left = kc;
+            else if (k == "P2_Right") neogeo::Config::Key::P2_Right = kc;
+            else if (k == "P2_A") neogeo::Config::Key::P2_ButtonA = kc;
+            else if (k == "P2_B") neogeo::Config::Key::P2_ButtonB = kc;
+            else if (k == "P2_C") neogeo::Config::Key::P2_ButtonC = kc;
+            else if (k == "P2_D") neogeo::Config::Key::P2_ButtonD = kc;
+            else if (k == "P1_Coin") neogeo::Config::Key::P1_Coin = kc;
+            else if (k == "P2_Coin") neogeo::Config::Key::P2_Coin = kc;
+            else if (k == "P1_Start") neogeo::Config::Key::P1_Start = kc;
+            else if (k == "P2_Start") neogeo::Config::Key::P2_Start = kc;
+            else if (k == "P1_Select") neogeo::Config::Key::P1_Select = kc;
+            else if (k == "P2_Select") neogeo::Config::Key::P2_Select = kc;
+            else if (k == "Test") neogeo::Config::Key::Test = kc;
+            else if (k == "Service") neogeo::Config::Key::Service = kc;
+        }
     }
 }
 
